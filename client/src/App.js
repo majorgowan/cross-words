c40ef375f63b60c771327caac18c362a70f37143
Mark Fruman <mark.fruman@yahoo.com>
2019-02-02 22:02:41 -0500

    adding titles to index.html, fixing == -> ===, and styling clue text

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div class="header">
-            {/* ref: https://css-tricks.com/snippets/svg/curved-text-along-path/ */}
-            <svg viewBox="0 0 800 80">
-                <path id="curve" d="M 150 100 C 250 30, 550 30, 650 100" />
-                <text id="header-text">
-                    <textPath id="#curve" text-anchor="middle" alignment-baseline="central" startOffset="50%">
-                    Cross Words!
-                    </textPath>
-                </text>
-            </svg>
-        </div>
         <CrossWord />
       </div>
     );
